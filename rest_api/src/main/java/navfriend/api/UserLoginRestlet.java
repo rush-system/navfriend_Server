0741a083dcc7537b3d3ad0a372bc0bd39cf7699d
krajetum <archimede96@hotmail.it>
2015-04-13 21:32:38 +0200

    v1 post

--- rest_api/src/main/java/navfriend/api/UserLoginRestlet.java
+++ rest_api/src/main/java/navfriend/api/UserLoginRestlet.java
@@ -1,5 +1,8 @@
 package navfriend.api;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.restlet.ext.json.JsonRepresentation;
 import org.restlet.representation.Representation;
 import org.restlet.resource.Post;
 import org.restlet.resource.ServerResource;
@@ -13,8 +16,12 @@ public class UserLoginRestlet extends ServerResource{
 
 	@Post("json")
 	public void Login(Representation entity){
-		
-
+		try {
+			JsonRepresentation jsonRep=(JsonRepresentation) entity;
+			JSONObject obj= jsonRep.getJsonObject();
+			System.out.println("Got " + obj);
+		}catch(JSONException ex){
+			ex.printStackTrace();
+		}
 	}
-
 }
